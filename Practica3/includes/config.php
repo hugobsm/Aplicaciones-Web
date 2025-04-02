38d69894fba1d48ae05eaec0e4557406dfe0faa3
victorialc10 <victorialdc10@gmail.com>
2025-04-02 18:37:40 +0200

    ee

--- Practica3/includes/config.php
+++ Practica3/includes/config.php
@@ -5,7 +5,7 @@ require_once("includes/application.php");
 
 define('BD_HOST', 'vm018.swarm.test');//vm018.swarm.test
 define('BD_NAME', 'brandswap');
-define('BD_USER', 'root'); // Ajusta según tu configuración
+define('BD_USER', 'brandswap'); // Ajusta según tu configuración
 define('BD_PASS', ''); // Si tienes contraseña, colócala aquí
 
 define('RAIZ_APP',__DIR__);
