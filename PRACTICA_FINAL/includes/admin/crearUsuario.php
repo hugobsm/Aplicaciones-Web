55e802e9d2f5f8914ad8ae7cf45f53d183700ea0
pablioliyu <pasis@ucm.es>
2025-04-29 19:54:56 +0200

    mejoras del admin

--- PRACTICA_FINAL/includes/admin/crearUsuario.php
+++ PRACTICA_FINAL/includes/admin/crearUsuario.php
@@ -2,6 +2,12 @@
 require_once(__DIR__ . '/../config.php');
 require_once(__DIR__ . "/../login/registerForm.php");
 
+// Comprobar si el usuario está autenticado y tiene el rol adecuado
+if ($_SESSION['tipo'] !== 'admin') {
+    // Si no es admin o no está autenticado, redirigir a la página de inicio
+    header("Location: /Aplicaciones-Web/PRACTICA_FINAL/index.php");
+    exit();    
+}
 $tituloPagina = 'Registro de usuario';
 
 $form = new registerForm(); 
--- PRACTICA_FINAL/includes/admin/eliminarUsuario.php
+++ PRACTICA_FINAL/includes/admin/eliminarUsuario.php
@@ -1,6 +1,14 @@
 <?php
 require_once __DIR__ . '/../usuario/userDAO.php';
 
+// Comprobar si el usuario está autenticado y tiene el rol adecuado
+if ($_SESSION['tipo'] !== 'admin') {
+    // Si no es admin o no está autenticado, redirigir a la página de inicio
+    header("Location: /Aplicaciones-Web/PRACTICA_FINAL/index.php");
+    exit();
+
+}
+
 /*if (session_status() === PHP_SESSION_NONE) session_start();
 
 if (!isset($_SESSION['usuario']['tipo']) || $_SESSION['usuario']['tipo'] !== 'admin') {
