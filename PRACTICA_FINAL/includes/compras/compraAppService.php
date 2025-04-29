2beb919a7efb73165641892a93eede46785e69e5
hugo boisseleau <boisseleau.hugo@gmail.com>
2025-04-29 13:43:05 +0200

    profile

--- PRACTICA_FINAL/includes/compras/compraAppService.php
+++ PRACTICA_FINAL/includes/compras/compraAppService.php
@@ -16,7 +16,31 @@ class compraAppService {
 
     public function realizarCompra($compraDTO) {
         $ICompraDAO = compraFactory::CreateCompra();
-        return $ICompraDAO->realizarCompra($compraDTO);
+        $resultado = $ICompraDAO->realizarCompra($compraDTO);
+
+        if ($resultado) {
+            // Obtenemos los IDs
+            $idComprador = $compraDTO->getIdUsuario();
+            $idVendedor = $compraDTO->getIdVendedor();
+            $idProducto = $compraDTO->getIdProducto();
+
+            // Obtenemos el precio del producto desde la BD
+            $conn = application::getInstance()->getConexionBd();
+            $stmt = $conn->prepare("SELECT precio FROM productos WHERE id_producto = ?");
+            $stmt->bind_param("i", $idProducto);
+            $stmt->execute();
+            $stmt->bind_result($precio);
+            $stmt->fetch();
+            $stmt->close();
+
+            // Actualizamos saldos
+            require_once(__DIR__ . '/../usuario/userAppService.php');
+            $userService = userAppService::GetSingleton();
+            $userService->restarSaldo($idComprador, $precio);
+            $userService->sumarSaldo($idVendedor, $precio);
+        }
+
+        return $resultado;
     }
 
     public function obtenerComprasPorUsuario($id_usuario) {
@@ -24,5 +48,4 @@ class compraAppService {
         return $ICompraDAO->obtenerComprasPorUsuario($id_usuario);
     }
 }
-
 ?>
